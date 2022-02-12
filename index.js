651e92dd775f9671eaa3aef11b1a6031ed0bb952
Alakhdeep singh <54256747+Alakhdeepsingh@users.noreply.github.com>
2022-02-13 00:35:07 +0530

    portfolio

--- index.js
+++ index.js
@@ -1,80 +1,75 @@
-var TxtRotate = function(el, toRotate, period) {
-  this.toRotate = toRotate;
-  this.el = el;
-  this.loopNum = 0;
-  this.period = parseInt(period, 10) || 1000;
-  this.txt = '';
-  this.tick();
-  this.isDeleting = false;
+var TxtRotate = function (el, toRotate, period) {
+	this.toRotate = toRotate;
+	this.el = el;
+	this.loopNum = 0;
+	this.period = parseInt(period, 10) || 1000;
+	this.txt = '';
+	this.tick();
+	this.isDeleting = false;
 };
 
-TxtRotate.prototype.tick = function() {
-  var i = this.loopNum % this.toRotate.length;
-  var fullTxt = this.toRotate[i];
+TxtRotate.prototype.tick = function () {
+	var i = this.loopNum % this.toRotate.length;
+	var fullTxt = this.toRotate[i];
 
-  if (this.isDeleting) {
-    this.txt = fullTxt.substring(0, this.txt.length - 1);
-  } else {
-    this.txt = fullTxt.substring(0, this.txt.length + 1);
-  }
+	if (this.isDeleting) {
+		this.txt = fullTxt.substring(0, this.txt.length - 1);
+	} else {
+		this.txt = fullTxt.substring(0, this.txt.length + 1);
+	}
 
-  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+	this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
-  var that = this;
-  var delta = 300 - Math.random() * 100;
+	var that = this;
+	var delta = 300 - Math.random() * 100;
 
-  if (this.isDeleting) { delta /= 2; }
+	if (this.isDeleting) {
+		delta /= 2;
+	}
 
-  if (!this.isDeleting && this.txt === fullTxt) {
-    delta = this.period;
-    this.isDeleting = true;
-  } else if (this.isDeleting && this.txt === '') {
-    this.isDeleting = false;
-    this.loopNum++;
-    delta = 500;
-  }
+	if (!this.isDeleting && this.txt === fullTxt) {
+		delta = this.period;
+		this.isDeleting = true;
+	} else if (this.isDeleting && this.txt === '') {
+		this.isDeleting = false;
+		this.loopNum++;
+		delta = 500;
+	}
 
-  setTimeout(function() {
-    that.tick();
-  }, delta);
+	setTimeout(function () {
+		that.tick();
+	}, delta);
 };
 
-window.onload = function() {
-  var elements = document.getElementsByClassName('txt-rotate');
-  for (var i=0; i<elements.length; i++) {
-    var toRotate = elements[i].getAttribute('data-rotate');
-    var period = elements[i].getAttribute('data-period');
-    if (toRotate) {
-      new TxtRotate(elements[i], JSON.parse(toRotate), period);
-    }
-  }
-  // INJECT CSS
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
-  document.body.appendChild(css);
+window.onload = function () {
+	var elements = document.getElementsByClassName('txt-rotate');
+	for (var i = 0; i < elements.length; i++) {
+		var toRotate = elements[i].getAttribute('data-rotate');
+		var period = elements[i].getAttribute('data-period');
+		if (toRotate) {
+			new TxtRotate(elements[i], JSON.parse(toRotate), period);
+		}
+	}
+
+	var css = document.createElement("style");
+	css.type = "text/css";
+	css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+	document.body.appendChild(css);
 };
 
-var state =0;
+var state = 0;
+
 function changeColor() {
-  let root = document.documentElement;
-  if(state == 0) {
-    root.style.setProperty("--kprimary", 'purple');
-    root.style.setProperty("--kprimary2", '#00203FFF');
-    root.style.setProperty("--kprimary3", 'white');
-    root.style.setProperty("--kheadings", '#00203FFF');
-    root.style.setProperty("--kprimary4", 'black');
-    
-    state++;
-  }
-  else {
-    window.location.reload();
-  }
+	let root = document.documentElement;
+	if (state == 0) {
+		root.style.setProperty("--kprimary", 'purple');
+		root.style.setProperty("--kprimary2", '#00203FFF');
+		root.style.setProperty("--kprimary3", 'white');
+		root.style.setProperty("--kheadings", '#00203FFF');
+		root.style.setProperty("--kprimary4", 'black');
+
+		state++;
+	} else {
+		window.location.reload();
+	}
 }
-
-
-
-
-
-
-
