d971934136b499102d4f1259ab1870a51bca8049
Alakhdeep singh <54256747+Alakhdeepsingh@users.noreply.github.com>
2022-02-19 23:19:16 +0530

    portfolio

--- index.js
+++ index.js
@@ -1,51 +1,104 @@
-var TxtRotate = function (el, toRotate, period) {
-	this.toRotate = toRotate;
-	this.el = el;
-	this.loopNum = 0;
-	this.period = parseInt(period, 10) || 1000;
-	this.txt = '';
-	this.tick();
-	this.isDeleting = false;
-};
+/*
+This function takes arguments elements[], toRotate and period
+*/
+class TxtRotate {
+	constructor(el, toRotate, period) {
+		// console.log("This is: " + this);
 
-TxtRotate.prototype.tick = function () {
-	var i = this.loopNum % this.toRotate.length;
-	var fullTxt = this.toRotate[i];
+		//array to rotate
+		this.toRotate = toRotate;
 
-	if (this.isDeleting) {
-		this.txt = fullTxt.substring(0, this.txt.length - 1);
-	} else {
-		this.txt = fullTxt.substring(0, this.txt.length + 1);
-	}
+		//list of elements
+		this.el = el;
+		this.loopNum = 0;
 
-	this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
-
-	var that = this;
-	var delta = 300 - Math.random() * 100;
-
-	if (this.isDeleting) {
-		delta /= 2;
-	}
-
-	if (!this.isDeleting && this.txt === fullTxt) {
-		delta = this.period;
-		this.isDeleting = true;
-	} else if (this.isDeleting && this.txt === '') {
+		//10 is for decimal
+		this.period = parseInt(period, 10) || 1000;
+		this.txt = '';
+		this.tick();
 		this.isDeleting = false;
-		this.loopNum++;
-		delta = 500;
 	}
+	tick() {
+		var i = this.loopNum % this.toRotate.length;
 
-	setTimeout(function () {
-		that.tick();
-	}, delta);
-};
+		// console.log(this.loopNum);
+		var fullTxt = this.toRotate[i];
+
+		//Contains present value from ['programmer','developer','designer']
+		// console.log(fullTxt);
+		if (this.isDeleting) {
+			this.txt = fullTxt.substring(0, this.txt.length - 1);
+			// console.log("Deleting" + this.txt + this.isDeleting);
+		} else {
+			this.txt = fullTxt.substring(0, this.txt.length + 1);
+		}
+
+
+		// console.log(this.el);
+		//Changes html document of given element. 
+		/*
+		value of this.el
+		<span class="txt-rotate" data-period="1000" data-rotate="[ "Programmer.", "Developer.", "Designer." ]">
+		</span>
+		*/
+		this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+		/*
+    
+		value of this.el after above line
+		<span class="txt-rotate" data-period="1000" data-rotate="[ "Programmer.", "Developer.", "Designer." ]">
+		<span class="wrap"> Programmer </span>
+		</span>
+		*/
+		//copy object
+		var that = this;
+		// console.log(that);
+
+		//Rendumness
+		var delta = 300 - Math.random() * 100;
+
+		/*
+		Delta is used to set time stamp for recalling tick function
+		*/
+		if (this.isDeleting) {
+			delta /= 2;
+			// console.log("Delta cruel:" + delta);
+		}
+
+		if (!this.isDeleting && this.txt === fullTxt) {
+			delta = this.period;
+			this.isDeleting = true;
+		} else if (this.isDeleting && this.txt === '') {
+			this.isDeleting = false;
+			this.loopNum++;
+			// console.log("loop number:" + this.loopNum);
+			delta = 500;
+		}
+
+		setTimeout(function () {
+			that.tick();
+		}, delta);
+	}
+}
+
+
+
+
+/*This function runs when the window is loaded. 
+txt-rotate is html <span> which contains one of '[ "Programmer.", "Developer.", "Designer." ]
+'[ "Programmer.", "Developer.", "Designer." ] is given in data-toggler in the same element.
+data-period is the time period to change between each of these values.
+element will contain <span> element
+*/
 
 window.onload = function () {
 	var elements = document.getElementsByClassName('txt-rotate');
 	for (var i = 0; i < elements.length; i++) {
 		var toRotate = elements[i].getAttribute('data-rotate');
+		// console.log(elements);
+		// console.log("toRotate" + toRotate);
 		var period = elements[i].getAttribute('data-period');
+		// console.log("period" + period)
+		// console.log(elements)
 		if (toRotate) {
 			new TxtRotate(elements[i], JSON.parse(toRotate), period);
 		}
@@ -69,6 +122,7 @@ function changeColor() {
 		root.style.setProperty("--kprimary4", 'black');
 
 		state++;
+		console.log(state + "state");
 	} else {
 		window.location.reload();
 	}
